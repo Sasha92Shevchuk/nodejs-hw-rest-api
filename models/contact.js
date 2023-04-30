aeaa19a23917f05655164078e6fc8086485713f4
Sasha92Shevchuk <108683139+Sasha92Shevchuk@users.noreply.github.com>
2023-04-30 13:58:50 +0300

    added work with mongodb

---
+++ models/contact.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+
+const emailRegexp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phoneRegexp = /^\(\d{3}\) \d{3}-\d{4}$/;
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: true,
+    },
+    phone: {
+      type: String,
+      match: phoneRegexp, // (111) 111-1111,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleMongooseError);
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  phone: Joi.string().pattern(phoneRegexp).required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+  Contact,
+  schemas,
+};
--- server.js
+++ server.js
@@ -1,5 +1,29 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// const DB_HOST =
+//   "mongodb+srv://oleksandr:JlOUAyWLy2k5VjgK@cluster0.d1goug0.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+// const { DB_HOST } = require("./config");
+const { DB_HOST, PORT = 3000 } = process.env;
+
+// mongoose.set('strictQuery', true) - якщо буде повідомлення в терміналі
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1); // команда закриває запущені процеси, 1-закрити з невідомою помилкою
+  });
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => console.log("Database connection successful"))
+//   .catch((error) => console.log(error.message));
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
