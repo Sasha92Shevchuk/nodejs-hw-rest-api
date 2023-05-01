b24fef706a7ff62e82c84145dee8f968a85782ea
Sasha92Shevchuk <108683139+Sasha92Shevchuk@users.noreply.github.com>
2023-05-01 17:41:18 +0300

    clean comments

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-// const DB_HOST =
-//   "mongodb+srv://oleksandr:JlOUAyWLy2k5VjgK@cluster0.d1goug0.mongodb.net/contacts_reader?retryWrites=true&w=majority";
-// const { DB_HOST } = require("./config");
 const { DB_HOST, PORT = 3000 } = process.env;
 
 // mongoose.set('strictQuery', true) - якщо буде повідомлення в терміналі
@@ -18,12 +15,3 @@ mongoose
     console.log(error.message);
     process.exit(1); // команда закриває запущені процеси, 1-закрити з невідомою помилкою
   });
-
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => console.log("Database connection successful"))
-//   .catch((error) => console.log(error.message));
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
